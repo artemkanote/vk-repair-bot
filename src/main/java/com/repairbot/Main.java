735dde4ad5588a6e8b49632239cae983c8a1a5e6
artemkanote <artemtarabakin23@gmail.com>
2026-03-23 12:08:11 +0300

    Create Main.java file in com.repairbot package

---
+++ src/main/java/com/repairbot/Main.java
@@ -0,0 +1,9 @@
+// Main class for the RepairBot application
+
+package com.repairbot;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, RepairBot!");
+    }
+}
